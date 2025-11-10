c77a7ca5c484cc08d038ef4ea3d697e5cdb61348
Achmad Farizi <farizi1412@gmail.com>
2025-11-10 14:29:05 +0700

    add views: tambah, kurang, bagi, kali

---
+++ app/Http/Controllers/BelajarController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BelajarController extends Controller
+{
+    public function index() {
+        return "Halo saya sedang belajar laravel";
+    }
+
+    public function create() {
+        return view('create');
+    }
+
+    public function tambah() {
+        return view('tambah');
+    }
+
+    public function kurang() {
+        return view('kurang');
+    }
+
+    public function bagi() {
+        return view('bagi');
+    }
+
+    public function kali() {
+        return view('kali');
+    }
+
+    public function tambahAction(Request $request) {
+        $angka1 = $request->angka1;
+        $angka2 = $request->post('angka2');
+
+        $jumlah = $angka1 + $angka2;
+        return view('tambah', compact('jumlah'));
+    }
+
+    public function kurangAction(Request $request) {
+        $angka1 = $request->angka1;
+        $angka2 = $request->post('angka2');
+
+        $jumlah = $angka1 - $angka2;
+        return view('kurang', compact('jumlah'));
+    }
+
+    public function kaliAction(Request $request) {
+        $angka1 = $request->angka1;
+        $angka2 = $request->post('angka2');
+
+        $jumlah = $angka1 * $angka2;
+        return view('kali', compact('jumlah'));
+    }
+
+    public function bagiAction(Request $request) {
+        $angka1 = $request->angka1;
+        $angka2 = $request->post('angka2');
+
+        if ($angka2 == 0) {
+            $jumlah = 'Error: pembagian dengan nol';
+        } else {
+            $jumlah = $angka1 / $angka2;
+        }
+
+        return view('bagi', compact('jumlah'));
+    }
+}
---
+++ resources/views/tambah.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah-tambahan</title>
+</head>
+
+<body>
+    <h3>Matematika Sederhana</h3>
+    <p>Pilih menu aritmatika dibawah ini</p>
+    <form action="{{route('tambah-action')}}" method="post">
+        @csrf
+        <label for="">Angka 1</label>
+        <input type="text" placeholder="Masukkan angka" name="angka1" required>
+        <br><br>
+        <label for="">Angka 2</label>
+        <input type="text" placeholder="Masukkan angka" name="angka2" required>
+        <br><br>
+        <button>Jumlahkan</button>
+    </form>
+
+    <h2>Jumlahnya adalah : {{$jumlah ?? 0}}</h2>
+</body>
+
+</html>
